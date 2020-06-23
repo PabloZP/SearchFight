c5e3e26877b41c22d408b8f5bca5fb9ad17a4be9
PabloZP <41882079+PabloZP@users.noreply.github.com>
2020-06-23 17:39:09 -0500

    Add files via upload

---
+++ PjChallenge/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace PjChallenge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.txtSearchRes = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtText
+            // 
+            this.txtText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtText.Location = new System.Drawing.Point(61, 53);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(254, 23);
+            this.txtText.TabIndex = 0;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(340, 49);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 1;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.BtnOk_Click);
+            // 
+            // txtSearchRes
+            // 
+            this.txtSearchRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearchRes.Location = new System.Drawing.Point(130, 129);
+            this.txtSearchRes.Multiline = true;
+            this.txtSearchRes.Name = "txtSearchRes";
+            this.txtSearchRes.Size = new System.Drawing.Size(387, 190);
+            this.txtSearchRes.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Search Results";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSearchRes);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txtText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.TextBox txtSearchRes;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PjChallenge/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BE;
+using BL;
+
+namespace PjChallenge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOk_Click(object sender, EventArgs e)
+        {         
+            Searching Sea = new Searching(txtText.Text);
+            if (Sea.getParsedOk())
+            {
+                if (Sea.bSearchOk)
+                {
+                    txtSearchRes.ForeColor = Color.Green;
+                }
+                else
+                {
+                    txtSearchRes.ForeColor = Color.Red;
+                }
+            }
+            else
+            {
+                txtSearchRes.ForeColor = Color.Red;
+            }
+            txtSearchRes.Text = Sea.Results();
+           
+            
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+       
+    }
+}
